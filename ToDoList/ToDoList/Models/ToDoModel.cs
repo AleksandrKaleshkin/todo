652a23d1bb2a81d95c66a6ad3e63885a2f26d35e
AleksandrKaleshkin <akaleskin@gmail.com>
2022-09-12 00:26:45 +0300

    ver.1

---
+++ ToDoList/ToDoList/Models/ToDoModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    class ToDoModel: INotifyPropertyChanged
+    {
+        private string _text;
+        private int _isDone;
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        public int IsDone
+        {
+            get { return _isDone; }
+            set
+            {
+                if (_isDone == value)
+                    return;
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+
+        public string  Text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+    }
+}
